0a882cae0e04ba626786bbeddfcd6bb93a708ccc
kameshcodes <kamesh.iitbombay@gmail.com>
2024-01-01 23:49:06 +0530

    adjustment in bilateral filtering

--- opencv/Intermediate/3_1_Bilateral_blurring.py
+++ opencv/Intermediate/3_1_Bilateral_blurring.py
@@ -19,10 +19,7 @@ params = [(11, 21, 7), (11, 41, 21), (11, 61, 39)]
 
 # loop over the diameter, sigma color, and sigma space
 for (diameter, sigmaColor, sigmaSpace) in params:
-	# apply bilateral filtering to the image using the current set of
-	# parameters
 	blurred = cv2.bilateralFilter(image, diameter, sigmaColor, sigmaSpace)
-	# show the output image and associated parameters
 	title = "Blurred d={}, sc={}, ss={}".format(
 		diameter, sigmaColor, sigmaSpace)
 	cv2.imshow(title, blurred)
