d18b75b0ffb396c56ffebeac009156661de9fce3
kameshcodes <kamesh.iitbombay@gmail.com>
2024-01-01 23:36:15 +0530

    Average Blurr added in Blurring and Smoothing

---
+++ opencv/Intermediate/1_kernels.py
@@ -0,0 +1,24 @@
+import numpy as np 
+import argparse
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", type=str, default="photo1.jpg", help = "path to the input image")
+args = vars(ap.parse_args())
+
+
+image = cv2.imread(args['image'])
+print(image.shape)
+image = cv2.resize(image, (500,300))
+cv2.imshow("Original", image)
+cv2.waitKey(0)
+
+
+kernelsizes = [(3,3), (9,9), (15,15)]
+
+# Average Blurr
+for (kX, kY) in kernelsizes:
+    blurred_img = cv2.blur(image, (kX, kY))
+    cv2.imshow(f"Average ({kX}, {kY})", blurred_img)
+    cv2.waitKey(0)
+cv2.destroyAllWindows()
