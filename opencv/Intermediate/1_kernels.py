a51531deb5369828c11736955948c4510fc7b348
kameshcodes <kamesh.iitbombay@gmail.com>
2024-01-01 23:37:17 +0530

    Median Blurr

--- opencv/Intermediate/1_kernels.py
+++ opencv/Intermediate/1_kernels.py
@@ -16,16 +16,23 @@ cv2.waitKey(0)
 
 kernelsizes = [(3,3), (9,9), (15,15)]
 
-# Average Blurr
+# Average Blur
 for (kX, kY) in kernelsizes:
     blurred_img = cv2.blur(image, (kX, kY))
     cv2.imshow(f"Average ({kX}, {kY})", blurred_img)
     cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-# Gaussian Blurr
+# Gaussian Blur
 for (kX, kY) in kernelsizes:
     blurred_img = cv2.GaussianBlur(image, (kX,kY), 0) #read parameters of this 
     cv2.imshow(f"Gaussian ({kX}, {kY})", blurred_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+# Median Blur 
+for k in (3, 9, 15):
+    blurred_img = cv2.medianBlur(image, k) 
+    cv2.imshow(f"Meadian K {k}", blurred_img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
